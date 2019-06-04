55872af122a7d7a3a2a1b1698c3b9e1dc88b2ba1
ddot727 <ddot727@gmail.com>
2019-06-04 16:56:40 -0400

    finally got bubble sort done, moving on to recursive sorting

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -17,8 +17,14 @@ def selection_sort(arr):
 
 # TO-DO:  implement the Bubble Sort function below
 def bubble_sort(arr):
-
-    return arr
+    while True:
+        done = False
+        for i in range(0, len(arr) - 1):
+            if arr[i+1] < arr[i]:
+                (arr[i], arr[i+1]) = (arr[i+1], arr[i])
+                done = True
+        if done != True:
+            return arr
 
 
 # STRETCH: implement the Count Sort function below
